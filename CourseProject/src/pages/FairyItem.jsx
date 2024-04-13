0f4c8469cd2b746b33e5b5feafa06713be43e7f9
NelleFavoreel <114076571+NelleFavoreel@users.noreply.github.com>
2024-04-13 14:07:43 +0200

    Fairy page done with dummy data

--- CourseProject/src/pages/FairyItem.jsx
+++ CourseProject/src/pages/FairyItem.jsx
@@ -1,10 +1,14 @@
+import Image from "../assets/picture.jpeg";
+
 function FairyItem() {
 	return (
-		<li>
-			<img src="#" alt="" />
-			<label>Naam Sprookje</label>
-			<label>Naam student</label>
-		</li>
+		<div className="box">
+			<img className="image" src={Image} alt="image" />
+			<li className="itemDetail">
+				<label>Naam Sprookje</label>
+				<label>Naam student</label>
+			</li>
+		</div>
 	);
 }
 export default FairyItem;
--- CourseProject/src/pages/ProjectsPage.jsx
+++ CourseProject/src/pages/ProjectsPage.jsx
@@ -2,16 +2,31 @@ import FairyItem from "./FairyItem";
 
 function ProjectPage() {
 	return (
-		<div>
-			<h2>Project</h2>
-			<p>Comming soon... 👉 🧜‍♀️</p>
-			<ul className="fairyItem">
-				<FairyItem></FairyItem>
-				<FairyItem></FairyItem>
-				<FairyItem></FairyItem>
-				<FairyItem></FairyItem>
-				<FairyItem></FairyItem>
-			</ul>
+		<div className="projectPage">
+			<div className="sortProject">
+				<input type="text" placeholder="Zoek sprookje" id="" />
+				<h2>Categroie</h2>
+				<button>Horror</button>
+				<button>Kind vriendelijk</button>
+				<button>Gebroeders Grim</button>
+				<button>Griekse verhalen</button>
+				<button>Kind vriendelijk</button>
+				<h2>Sorteren op</h2>
+				<button>Nieuwste</button>
+				<button>Oudste</button>
+				<h2>Beste beoordeeld</h2>
+				<button>Hoogste</button>
+				<button>Laagste</button>
+			</div>
+			<div>
+				<ul className="fairyItem">
+					<FairyItem></FairyItem>
+					<FairyItem></FairyItem>
+					<FairyItem></FairyItem>
+					<FairyItem></FairyItem>
+					<FairyItem></FairyItem>
+				</ul>
+			</div>
 		</div>
 	);
 }
