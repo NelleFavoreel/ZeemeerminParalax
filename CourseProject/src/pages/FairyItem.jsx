039d83d8901ee72813e2fa7e4e0f82bc8298cbe6
NelleFavoreel <114076571+NelleFavoreel@users.noreply.github.com>
2024-04-13 13:04:36 +0200

    Made items for fairy

---
+++ CourseProject/src/pages/FairyItem.jsx
@@ -0,0 +1,10 @@
+function FairyItem() {
+	return (
+		<li>
+			<img src="#" alt="" />
+			<label>Naam Sprookje</label>
+			<label>Naam student</label>
+		</li>
+	);
+}
+export default FairyItem;
--- CourseProject/src/pages/ProjectsPage.jsx
+++ CourseProject/src/pages/ProjectsPage.jsx
@@ -1,8 +1,17 @@
+import FairyItem from "./FairyItem";
+
 function ProjectPage() {
 	return (
 		<div>
 			<h2>Project</h2>
 			<p>Comming soon... 👉 🧜‍♀️</p>
+			<ul className="fairyItem">
+				<FairyItem></FairyItem>
+				<FairyItem></FairyItem>
+				<FairyItem></FairyItem>
+				<FairyItem></FairyItem>
+				<FairyItem></FairyItem>
+			</ul>
 		</div>
 	);
 }
