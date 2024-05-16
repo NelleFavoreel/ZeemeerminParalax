74773bbb798c6868380088b0b4a39ddcc3cb7c0d
NelleFavoreel <114076571+NelleFavoreel@users.noreply.github.com>
2024-05-16 15:53:11 +0200

    delete css

--- course-project/src/App.jsx
+++ course-project/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import "./App.css";
-import "./Fairy.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 <link rel="preconnect" href="https://fonts.googleapis.com"></link>;
 <link rel="preconnect" href="https://fonts.googleapis.com"></link>;
