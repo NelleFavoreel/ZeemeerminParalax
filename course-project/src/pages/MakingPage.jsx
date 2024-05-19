4629d33788fd92f153ceb9a67c2044333cf6150a
NelleFavoreel <114076571+NelleFavoreel@users.noreply.github.com>
2024-05-19 11:26:48 +0200

    add extra info on the makingPage

--- course-project/src/pages/MakingPage.jsx
+++ course-project/src/pages/MakingPage.jsx
@@ -17,6 +17,12 @@ function MakingPage() {
 						Voor het vak frontend moeten we een website maken, inclusief een website over een sprookje. Ik heb gekozen voor het sprookje De Kleine Zeemeermin. Ik vond dit sprookje altijd al erg leuk, en ik vind de onderwaterwereld altijd heel
 						interessant.
 					</p>
+					<p>
+						De volgende zaken heb ik allemaal gebruikt: <br /> <br /> React: Een JavaScript-bibliotheek voor het bouwen van gebruikersinterfaces. <br /> React Router: Voor het beheren van de navigatie en routing binnen de applicatie. <br />{" "}
+						React-spring: Een bibliotheek voor animaties in React, gebruikt voor het implementeren van het parallax-effect. <br /> CSS: Voor de opmaak en het stylen van de componenten. <br /> JavaScript (ES6+): Voor het schrijven van de logica en
+						functionaliteit van de applicatie. <br /> JSON: Voor het opslaan en ophalen van de gegevens van de sprookjes. <br /> React hooks: Zoals useState en useEffect voor het beheren van de componenttoestand en bijwerkingen. <br /> React-spring
+						hooks: Zoals useSpring en useInView voor animaties en scroll-triggered effecten.
+					</p>
 					<h2>
 						By <br />
 						Nelle Favoreel
